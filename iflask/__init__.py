4bcf30f66d68305001928a6ad463dfe4ebf0cf59
Vitalii Hoshchenko <viposha@gmail.com>
2022-06-13 15:19:20 +0300

    Install Migrate pack to upgrading db

--- iflask/__init__.py
+++ iflask/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'e993ef007102772e684ad667a53c839d'
@@ -9,8 +10,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+migrate = Migrate(app, db)
 
-from .models import User, Post
-db.create_all()
+# from .models import User, Post
+# db.create_all()
 
 from iflask import routes
