96840f0e8bc2b8299edbec1d9b56b0cbdd79f261
Vitalii Hoshchenko <viposha@gmail.com>
2022-07-01 23:39:28 +0300

    add avatar to home page

--- iflask/routes.py
+++ iflask/routes.py
@@ -12,10 +12,10 @@ from iflask.models import User, Post
 
 
 @app.route('/')
-@app.route('/home')
+@app.route('/home', methods=['GET', 'POST'])
 def home():
-	posts = Post.query.order_by(Post.date_posted.desc())
-	return render_template('home.html', posts=posts)
+    posts = Post.query.order_by(Post.date_posted.desc())
+    return render_template("home.html", posts=posts)
 
 
 @app.route('/about')
